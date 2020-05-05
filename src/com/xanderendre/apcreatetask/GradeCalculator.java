0a6c459ccf1875b91fb0ef1c9f54c12ce1d0dc0f
Alex <shadyalexcodes@gmail.com>
2020-05-05 13:22:00 -0700

    Bug Fixes

--- src/com/xanderendre/apcreatetask/GradeCalculator.java
+++ src/com/xanderendre/apcreatetask/GradeCalculator.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class GradeCalculator {
 
-    public static final Map<String, Double> letterGradeToGPA = new HashMap<>();
+    public static final Map<String, Double> letterGradeToGPA = new LinkedHashMap<>();
+    public static final Map<String, String> sampleGradeRange = new LinkedHashMap<>();
     public static final Map<String, Double> classInformation = new HashMap<>();
     public static int totalClasses = 1;
 
@@ -13,22 +14,28 @@ public class GradeCalculator {
     }
 
     /**
-     * @author Xander Endre
      * Creates the student, prints the values, calculates the GPA
      */
     public static void startUp() {
         setLetterGradeToGPA();
+        setSampleGradeRange();
+        System.out.println(">>-------- GPA Calculator --------<<");
         System.out.println("Welcome to the AP CSP Create Task GPA Calculator");
+        System.out.println("Before you calculate your GPA we need a little bit more information.");
         wait(4000);
         try (Scanner scanner = new Scanner(System.in)) {
             Student student = new Student();
             getStudentInformation(student, scanner);
             // Fix the table
-            System.out.println("Here is a table of score ranges");
-            for (Map.Entry m : letterGradeToGPA.entrySet()) {
-                System.out.println(m.getKey() + " " + m.getValue());
+            System.out.println(">>-------- GPA Calculator --------<<");
+            System.out.println("In a moment you will see a table of score ranges. Choose the correlating letter based upon your score.");
+            wait(2000);
+            for (Map.Entry m : sampleGradeRange.entrySet()) {
+                System.out.println(m.getKey() + "  | " + m.getValue());
             }
 
+            System.out.println(">>-------- GPA Calculator --------<<");
+            System.out.println("For each of your " + student.numOfClasses + " class(es) please state the class name, and your score received.");
             wait(2000);
 
             while (totalClasses != student.numOfClasses + 1) {
@@ -48,23 +55,23 @@ public class GradeCalculator {
 
     /**
      * @param student
-     * @param scanner
-     * @author Xander Endre
-     * <p>
-     * Prompts the user for their classes name, and the letter grade of that class.
-     * Checks if the value inputted is valid, and then places this information into
-     * a HashMap.
+     * @param scanner Prompts the user for their classes name, and the letter grade of that class.
+     *                Checks if the value inputted is valid, and then places this information into
+     *                a HashMap.
      */
 
     public static void loadClassInformation(Student student, Scanner scanner) {
         Classes classes = new Classes();
+
+
         System.out.println("Please enter the name of your " + totalClasses + " class:");
         classes.className = scanner.nextLine();
         System.out.println("Please enter the letter grade you received for the class " + classes.className + ":");
         classes.classGrade = scanner.nextLine();
         double gradeValue = 0;
-        if (letterGradeToGPA.containsKey(classes.classGrade)) {
-            gradeValue = letterGradeToGPA.get(classes.classGrade);
+        // fix case
+        if (letterGradeToGPA.containsKey(classes.classGrade.toUpperCase())) {
+            gradeValue = letterGradeToGPA.get(classes.classGrade.toUpperCase());
         } else {
             System.out.println("Invalid Grade Inputted");
         }
@@ -75,8 +82,6 @@ public class GradeCalculator {
     /**
      * @param student
      * @return gpa
-     * @author Xander Endre
-     * <p>
      * Calcuates the overall GPA from the student.
      * Inside the for loop it takes the value from each map key and addes it to the class total
      */
@@ -93,8 +98,6 @@ public class GradeCalculator {
     }
 
     /**
-     * @author Xander Endre
-     * <p>
      * Adds all letter grades to the Hashmap
      */
     public static void setLetterGradeToGPA() {
@@ -113,19 +116,38 @@ public class GradeCalculator {
     }
 
     /**
+     * Adds all letter grades to the Hashmap
+     */
+    public static void setSampleGradeRange() {
+        sampleGradeRange.put("A+", "97 - 100");
+        sampleGradeRange.put("A ", "93 - 96");
+        sampleGradeRange.put("A-", "90 - 92");
+        sampleGradeRange.put("B+", "87 - 89");
+        sampleGradeRange.put("B ", "83 - 86");
+        sampleGradeRange.put("B-", "80 - 82");
+        sampleGradeRange.put("C+", "77 - 79");
+        sampleGradeRange.put("C ", "73 - 76");
+        sampleGradeRange.put("C-", "70 - 72");
+        sampleGradeRange.put("D+", "67 - 69");
+        sampleGradeRange.put("D ", "65 - 66");
+        sampleGradeRange.put("F ", "Below 65");
+    }
+
+    /**
      * @param student
-     * @param scanner
-     * @author Xander Endre
-     * <p>
-     * Creates the student inside of the student class and allows easy access to information
+     * @param scanner Creates the student inside of the student class and allows easy access to information
      */
 
     private static void getStudentInformation(Student student, Scanner scanner) {
-        System.out.println("Enter your first name: ");
+        System.out.println(">>-------- GPA Calculator --------<<");
+        System.out.println("Hello! What is your name?");
         student.firstName = scanner.nextLine();
-        System.out.println("Please enter the total number of classes");
+        System.out.println("How many classes do you have? (Please enter a integer)");
         student.numOfClasses = scanner.nextInt();
         scanner.nextLine();
+        System.out.println("Nice to meet you " + student.firstName + ". Let's get some more information about your classes");
+        wait(1000);
+
     }
 
 
@@ -175,6 +197,9 @@ Step One:
     Then i created a function to load the classes information.
     I put a table in to list all the grade values
     Then i added a wait function
+    Then I created the calculate gpa function
+    Then I went back and made the calculator look nice
+    then i ran into the issue where no matter what grade being entered was invalid
 
 Started by writing everything to command line.
 
